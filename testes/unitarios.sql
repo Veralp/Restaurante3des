5912daba97c28f0dbdf691c556f64dec0d7593ba
wellifabio <wellifabio@hotmail.com>
2023-08-03 09:57:28 -0300

    testes

--- testes/unitarios.sql
+++ testes/unitarios.sql
@@ -1,16 +1,17 @@
--- Popular o banco de dados com tetes
--- Tabela de Clientes
+-- Popular o banco de dados com alguns dados de tetes
 insert into cliente values
 (default, "443.785.553-10", "Jaqueline Souza Martim","19-85916-8673, 19-70565-8763", "13917-152", "105", null),
 (default, "789.588.411-50", "Sara Bispo Filho","19-04431-8940", "13917-052", "1025", null),
 (default, "731.715.974-38", "Clóvis Martins Bispo","19-20609-9645", "13917-156", "1150", "Fundos"),
 (default, "498.084.178-33", "Gilberto Martins Teixeira","19-34251-1302", "13917-158", "1138", "Ap144 bl2"),
-(default, "066.453.810-00", "Saulo Martim","19-50210-7927, 19-54920-8113", "13914-552", "1227", null);
+(default, "066.453.810-00", "Saulo Martim","19-50210-7927, 19-54920-8113", "13914-552", "1227", null),
+(default, "000.000.000-00", "Comer no local","19 99999-9999", "13917-151", "0", null);
 
 insert into motoboy values
 (default,"Gilberto Martins","JQT-7I87","CG Titan KS 150 Honda"),
 (default,"Clóvis Martins","FZC-9X29","Faser 250 Yamaha"),
-(default,"Saulo Martim","LHW-1J81","Ginerae");
+(default,"Saulo Martim","LHW-1J81","Ginerae"),
+(default,"Sem entrega","AAA-0A00","Sem veículo");
 
 insert into cardapio values
 (default, "Parmegiana de Frango", "Filé de frango empanado, coberto com molho de tomate, presunto e queijo mussarela gratinado. Acompanha arroz branco e fritas.", 25.00),
@@ -25,5 +26,15 @@ insert into cardapio values
 (default, "Cerveja de 600ml", "Cerveja de 600ml", 8.00),
 (default, "Cerveja de 350ml", "Cerveja de 350ml", 5.00);
 
+insert into pedido values
+(default, 1, 1, curtime(), DATE_ADD(curtime(), INTERVAL 10 minute), DATE_ADD(curtime(), INTERVAL 25 minute), 0, 5),
+(default, 2, 2, curtime(), DATE_ADD(curtime(), INTERVAL 15 minute), null, 0, 5);
+(default, 6, 4, curtime(), null, null, 0, 0);
+insert into itens values
+(default,1, 3, 1, (select preco from cardapio where id = 3)),
+(default,1, 8, 1, (select preco from cardapio where id = 8));
 
 
+
+update pedido set dataCozinha = curtime(), dataEntrega = curtime() where id = 1;
+update pedido set valorPedido = (select sum(valor) from itens where pedidoId = 1) where id = 1;
