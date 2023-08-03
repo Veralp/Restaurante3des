0273bb4b5b08fd86e83653059669f7359ae2eef9
wellifabio <wellifabio@hotmail.com>
2023-08-03 11:39:04 -0300

    motoboy crud

--- api/src/controllers/motoboy.controller.js
+++ api/src/controllers/motoboy.controller.js
@@ -6,4 +6,37 @@ const read = async (req, res) => {
     return res.json(motoboys);
 }
 
-module.exports = { read };
+const create = async (req, res) => {
+    const data = req.body;
+    const motoboy = await prisma.motoboy.create({
+        data: data
+    });
+    return res.status(201).json(motoboy).end();
+}
+
+const update = async (req, res) => {
+    const data = req.body;
+    let motoboy = await prisma.motoboy.update({
+        data: data,
+        where: {
+            id: parseInt(req.body.id)
+        }
+    });
+    res.status(202).json(motoboy).end();
+}
+
+const del = async (req, res) => {
+    let motoboy = await prisma.motoboy.delete({
+        where: {
+            id: parseInt(req.params.id)
+        }
+    });
+    res.status(204).json(motoboy).end();
+}
+
+module.exports = {
+    read,
+    create,
+    update,
+    del
+};
--- api/src/routes.js
+++ api/src/routes.js
@@ -5,9 +5,9 @@ const router = express.Router();
 const motoboy = require('./controllers/motoboy.controller');
 
 router.get('/', (req, res) => { return res.json("API Restaurante respondendo") });
-//router.post('/motoboy', motoboy.create);
+router.post('/motoboy', motoboy.create);
 router.get('/motoboy', motoboy.read);
-//router.put('/motoboy/:id', motoboy.update);
-//router.delete('/motoboy/:id', motoboy.remove);
+router.put('/motoboy', motoboy.update);
+router.delete('/motoboy/:id', motoboy.del);
 
 module.exports = router;
