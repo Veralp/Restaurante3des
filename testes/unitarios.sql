24113a8a180023705823f2148fba192e156ee7d9
wellifabio <wellifabio@hotmail.com>
2023-08-03 09:02:29 -0300

    testes

--- testes/unitarios.sql
+++ testes/unitarios.sql
@@ -6,3 +6,24 @@ insert into cliente values
 (default, "731.715.974-38", "Clóvis Martins Bispo","19-20609-9645", "13917-156", "1150", "Fundos"),
 (default, "498.084.178-33", "Gilberto Martins Teixeira","19-34251-1302", "13917-158", "1138", "Ap144 bl2"),
 (default, "066.453.810-00", "Saulo Martim","19-50210-7927, 19-54920-8113", "13914-552", "1227", null);
+
+insert into motoboy values
+(default,"Gilberto Martins","JQT-7I87","CG Titan KS 150 Honda"),
+(default,"Clóvis Martins","FZC-9X29","Faser 250 Yamaha"),
+(default,"Saulo Martim","LHW-1J81","Ginerae");
+
+insert into cardapio values
+(default, "Parmegiana de Frango", "Filé de frango empanado, coberto com molho de tomate, presunto e queijo mussarela gratinado. Acompanha arroz branco e fritas.", 25.00),
+(default, "Parmegiana de Carne", "Filé mignon empanado, coberto com molho de tomate, presunto e queijo mussarela gratinado. Acompanha arroz branco e fritas.", 30.00),
+(default, "Contra Filé Argentino", "Contra filé grelhado, com molho de churrasco, farofa, Acompanha arroz branco e feijão.", 50.00),
+(default, "Contra Filé Acebolado", "Contra filé grelhado, com molho de churrasco, farofa, Acompanha arroz branco e feijão.", 45.00),
+(default, "Bisteca de porco", "Bisteca de porco grelhada, farofa, couve refogada, Acompanha arroz branco, feijão e torresmo.", 49.90),
+(default, "Refrigerante de 2L", "Refrigerante de 2L", 15.00),
+(default, "Refrigerante de 1L", "Refrigerante de 1L", 12.00),
+(default, "Refrigerante de 600ml", "Refrigerante de 600ml", 8.00),
+(default, "Refrigerante de 350ml", "Refrigerante de 350ml", 5.00),
+(default, "Cerveja de 600ml", "Cerveja de 600ml", 8.00),
+(default, "Cerveja de 350ml", "Cerveja de 350ml", 5.00);
+
+
+
