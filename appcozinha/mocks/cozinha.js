386a1d2ab24b19b5e7d664b65ee2076d319206e0
wellifabio <wellifabio@hotmail.com>
2023-08-08 10:53:38 -0300

    app

---
+++ appcozinha/mocks/cozinha.js
@@ -0,0 +1,53 @@
+[
+    {
+        "id": 11,
+        "clienteId": 1,
+        "motoboyId": 1,
+        "dataPedido": "2023-08-08T09:05:21.000Z",
+        "dataCozinha": null,
+        "dataEntrega": null,
+        "valorPedido": 15,
+        "valorEntrega": 0,
+        "itens": [
+            {
+                "quantidade": 2,
+                "cardapio": {
+                    "id": 2,
+                    "produto": "Parmegiana de Carne",
+                    "descricao": "Filé mignon empanado, coberto com molho de tomate, presunto e queijo mussarela gratinado. Acompanha arroz branco e fritas.",
+                    "preco": 30
+                }
+            },
+            {
+                "quantidade": 1,
+                "cardapio": {
+                    "id": 1,
+                    "produto": "Parmegiana de Frango",
+                    "descricao": "Filé de frango empanado, coberto com molho de tomate, presunto e queijo mussarela gratinado. Acompanha arroz branco e fritas.",
+                    "preco": 25
+                }
+            }
+        ]
+    },
+    {
+        "id": 12,
+        "clienteId": 1,
+        "motoboyId": 1,
+        "dataPedido": "2023-08-08T12:56:20.769Z",
+        "dataCozinha": null,
+        "dataEntrega": null,
+        "valorPedido": 0,
+        "valorEntrega": 0,
+        "itens": [
+            {
+                "quantidade": 1,
+                "cardapio": {
+                    "id": 3,
+                    "produto": "Contra Filé Argentino",
+                    "descricao": "Contra filé grelhado, com molho de churrasco, farofa, Acompanha arroz branco e feijão.",
+                    "preco": 50
+                }
+            }
+        ]
+    }
+]
