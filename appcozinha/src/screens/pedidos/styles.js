e94b13571aaabf0ed3e5ca0a40a04fdf25cb6f56
wellifabio <wellifabio@hotmail.com>
2023-08-08 11:43:52 -0300

    app

--- appcozinha/src/screens/pedidos/styles.js
+++ appcozinha/src/screens/pedidos/styles.js
@@ -7,6 +7,28 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    pedido: {
+        backgroundColor: '#fff',
+        borderWidth: 1,
+        borderColor: '#DDD',
+        borderRadius: 5,
+        padding: 10,
+        marginBottom: 10,
+        width: 300,
+    },
+    text: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#333',
+    },
+    button: {
+        height: 42,
+        backgroundColor: '#FFBB00',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 2,
+        marginTop: 10,
+    },
 });
 
 export default styles;
